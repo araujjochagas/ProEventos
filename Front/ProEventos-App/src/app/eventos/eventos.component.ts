385984ed64c2cc4aadaa6505fad182b695d193f0
oimarkim <araujjo.chagas@gmail.com>
2021-10-01 19:32:13 -0300

    01102021

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any = [];
+  public eventosFiltrados: any = [];
   larguraImg: number = 150;
   margemImg:number = 2;
   exibirImg = true;
+  private _filtrolista: string = '';
+
+  public get filtrolista(): string {
+    return this._filtrolista;
+  }
+
+  public set filtrolista(value){
+    this._filtrolista = value;
+    this.eventosFiltrados = this.filtrolista ?
+                   this.filtrarEventos(this.filtrolista) :
+                   this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string; local: string }) =>
+       evento.tema.toLowerCase().indexOf(filtrarPor) !== -1 ||
+       evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -25,8 +47,11 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/Eventos/').subscribe(
-      response => this.eventos = response,
-      error => console.log(error),
+      response => {
+          this.eventos = response;
+          this.eventosFiltrados = this.eventos
+      },
+      error => console.log(error)
     );
 
   }
